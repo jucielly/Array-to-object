5c28e1cd027e3888f635873c82efd6cea111afa1
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-10-01 11:09:48 -0300

    first commit

--- productStructure.js
+++ productStructure.js
@@ -1,18 +1,31 @@
 const products = [
-  "preto-PP", 
-  "preto-M", 
-  "preto-G", 
-  "preto-GG", 
-  "preto-GG", 
-  "branco-PP", 
-  "branco-G", 
-  "vermelho-M", 
-  "azul-XG", 
-  "azul-XG", 
-  "azul-XG", 
+  "preto-PP",
+  "preto-M",
+  "preto-G",
+  "preto-GG",
+  "preto-GG",
+  "branco-PP",
+  "branco-G",
+  "vermelho-M",
+  "azul-XG",
+  "azul-XG",
+  "azul-XG",
   "azul-P"
 ]
 
-module.exports = () => {
-  // Código aqui
-}
+
+
+const productStructure = () => products.reduce((previous, product) => {
+  const [color, size] = product.split("-")
+
+  if (!previous[color]) {
+    previous[color] = {}
+  }
+  previous[color][size] = previous[color][size] + 1 || 1
+
+  return previous
+}, {})
+
+module.exports = productStructure
+
+console.log(productStructure())
